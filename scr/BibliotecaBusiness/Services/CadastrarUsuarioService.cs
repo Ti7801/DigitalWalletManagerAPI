83dc38de8872cce0dbd42a6f80ea91c3fadf899d
Ti7801 <tiagodaltro19@gmail.com>
2025-05-09 20:18:47 -0300

    Criação dos serviços CadastrarUsuarioService e ServiceResult.

---
+++ scr/BibliotecaBusiness/Services/CadastrarUsuarioService.cs
@@ -0,0 +1,59 @@
+﻿using BibliotecaBusiness.Abstractions;
+using BibliotecaBusiness.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaBusiness.Services
+{
+    public class CadastrarUsuarioService
+    {
+        private readonly ILogger<CadastrarUsuarioService> _logger;
+        private readonly UserManager<Usuario> _userManager;
+
+
+        public CadastrarUsuarioService(ILogger<CadastrarUsuarioService> logger, UserManager<Usuario> userManager)
+        {
+            _logger = logger;
+            _userManager = userManager;
+        }
+
+        public async Task<ServiceResult<IdentityResult?>> CreateIdentityUser(Usuario user, string password)
+        {
+            ServiceResult<IdentityResult?> serviceResult = new ServiceResult<IdentityResult?>();
+
+            try
+            {
+                var result = await _userManager.CreateAsync(user, password);
+                serviceResult.Value = result;
+ 
+                if (result.Succeeded)
+                {
+                    serviceResult.Sucess = true;
+                }
+                else
+                {
+                    serviceResult.Sucess = false;                 
+
+                    foreach (var error in result.Errors)
+                    {
+                        serviceResult.Erros.Add($"{error.Code}: {error.Description}");
+                    }
+                }
+            }
+            catch (Exception ex) 
+            {
+                serviceResult.Sucess = false;
+                serviceResult.Erros.Add($"Exceção inesperada: {ex.Message}");
+                _logger.LogError(ex, "Erro ao tentar criar o usuário!");
+            }
+
+            return serviceResult;
+        }
+    }
+}
