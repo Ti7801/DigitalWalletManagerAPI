f1af617c0516791990c7282c66c1c2e376b72fe2
Ti7801 <tiagodaltro19@gmail.com>
2025-05-08 21:49:57 -0300

    Configurando a TableConfig de Usuario.

---
+++ scr/BibliotecaData/TableConfig/UsuarioTableConfig.cs
@@ -0,0 +1,22 @@
+﻿using BibliotecaBusiness.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaData.TableConfig
+{
+    public class UsuarioTableConfig : IEntityTypeConfiguration<Usuario>
+    {   
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.HasMany<CarteiraDigital>()
+                .WithOne()
+                .HasForeignKey(carteiraDigital => carteiraDigital.UsuarioId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }     
+    }
+}
