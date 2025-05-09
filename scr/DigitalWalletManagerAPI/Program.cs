d98e7141ae367ff634638b907f86352aec37fbd1
Ti7801 <tiagodaltro19@gmail.com>
2025-05-08 21:54:10 -0300

    Realizando as primeiras configurações para estabelecer a conexão com o banco de dados no arquivo program.cs e iniciando a configuração de autenticação e autorização.

--- scr/DigitalWalletManagerAPI/Program.cs
+++ scr/DigitalWalletManagerAPI/Program.cs
@@ -1,3 +1,7 @@
+using BibliotecaBusiness.Abstractions;
+using BibliotecaData.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,29 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+
+builder.Services.AddDbContext<AppDbContext>(
+    (DbContextOptionsBuilder optionsBuilder) =>
+    {
+        string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+        optionsBuilder.UseNpgsql(connectionString);
+    },
+    ServiceLifetime.Scoped
+);
+
+builder.Services.AddScoped<ICarteiraDigitalRepository, CarteiraDiditalRepository>();
+
+
+
+builder.Services.AddScoped<ITransferenciaRepository, TransferenciaRepository>();
+
+
+
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+
+
 
 var app = builder.Build();
 
