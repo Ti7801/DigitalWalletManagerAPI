710c2ac5f992bf0f1712bcaa7402b6fb2a753a78
Ti7801 <tiagodaltro19@gmail.com>
2025-05-11 17:25:39 -0300

    Criando o novo repositório TransferenciaRepository.

--- scr/BibliotecaData/Data/TransferenciaRepository.cs
+++ scr/BibliotecaData/Data/TransferenciaRepository.cs
@@ -1,4 +1,8 @@
 ﻿using BibliotecaBusiness.Abstractions;
+using BibliotecaBusiness.DTOs;
+using BibliotecaBusiness.Exceptions;
+using BibliotecaBusiness.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +13,37 @@ namespace BibliotecaData.Data
 {
     public class TransferenciaRepository  : ITransferenciaRepository
     {
+        private readonly AppDbContext _appDbContext;    
+
+        public TransferenciaRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task<Transferencia?> CriarTransferencia(Transferencia transferencia)
+        {
+            _appDbContext.Transferencias.Add(transferencia);
+            await _appDbContext.SaveChangesAsync();
+
+            return transferencia;
+        }
+
+
+        public async Task<List<Transferencia>> ObterTransferenciasPorCarteiraAsync(Guid carteiraId, DateTime? dataInicio, DateTime? dataFim)
+        {
+            var query = _appDbContext.Transferencias.Where(t => t.CarteiraRemetenteId == carteiraId);
+
+            if (dataInicio.HasValue)
+            {
+                query = query.Where(t => t.Data >= dataInicio.Value);
+            }
+
+            if (dataFim.HasValue)
+            {
+                query = query.Where(t => t.Data <= dataFim.Value);
+            }
+
+            return await query.ToListAsync();
+        }
     }
 }
