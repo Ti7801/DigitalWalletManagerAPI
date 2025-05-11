cdd513319f210cd0e7ffb1a967715c98638f5ab8
Ti7801 <tiagodaltro19@gmail.com>
2025-05-11 18:07:55 -0300

    Criando um novo Dto para AdicionarSaldo.

---
+++ scr/BibliotecaBusiness/DTOs/AdicionarSaldoDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaBusiness.DTOs
+{
+    public class AdicionarSaldoDto
+    {
+        public Guid UsuarioId { get; set; }
+        public decimal Valor {  get; set; }
+        public Guid CarteiraId { get; set; }
+    }
+}
