aa06eb95f126614ef4cba3dd74cc542e94d1054e
Ti7801 <tiagodaltro19@gmail.com>
2025-05-11 17:27:33 -0300

    Readaptando as rotas das Actions e fazendo pequenos ajustes referente a um novo objeto do tipo CarteiraDigital criado.

--- scr/DigitalWalletManagerAPI/Controllers/CarteiraDigitalController.cs
+++ scr/DigitalWalletManagerAPI/Controllers/CarteiraDigitalController.cs
@@ -1,4 +1,5 @@
-﻿using BibliotecaBusiness.Exceptions;
+﻿using BibliotecaBusiness.DTOs;
+using BibliotecaBusiness.Exceptions;
 using BibliotecaBusiness.Models;
 using BibliotecaBusiness.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -7,7 +8,7 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 namespace DigitalWalletManagerAPI.Controllers
 {
     [ApiController]
-    [Route("api/carteiracontroller")]
+    [Route("api/carteira")]
     public class CarteiraDigitalController : ControllerBase
     {
         private readonly CriarCarteiraDigitalService _criarCarteiraDigitalService;
@@ -24,16 +25,23 @@ namespace DigitalWalletManagerAPI.Controllers
 
         }
 
-        [HttpPost]
+        [HttpPost("criar-carteira")]
         [ProducesResponseType(typeof(CarteiraDigital), StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult> CriarCarteiraDigital(CarteiraDigital carteira)
+        public async Task<ActionResult<CarteiraDigital>> CriarCarteiraDigital(CriarCarteiraDigitalDto carteiraDto)
         {
             if (!ModelState.IsValid)
             {
                 var errors = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).SingleOrDefault();
                 return BadRequest(errors);
             }
+
+            var carteira = new CarteiraDigital
+            {
+                Id = Guid.NewGuid(),    
+                Saldo = 0m,
+                UsuarioId = carteiraDto.UsuarioId,  
+            };
 
             ServiceResult serviceResult = await _criarCarteiraDigitalService.CadastrarCarteiraDigital(carteira);
 
@@ -42,13 +50,13 @@ namespace DigitalWalletManagerAPI.Controllers
                 return Problem(statusCode:StatusCodes.Status500InternalServerError);
             }
 
-            return CreatedAtAction(nameof(CriarCarteiraDigital), carteira.Id);
+            return CreatedAtAction(nameof(CriarCarteiraDigital), carteira);
         }
 
-        [HttpGet("{usuarioId:Guid}/{carteiraId:Guid}")]
+        [HttpGet("consultar/{usuarioId:Guid}/{carteiraId:Guid}")]
         [ProducesResponseType(typeof(decimal?), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<decimal?>> ConsultarSaldoDaCarteira(Guid usuarioId, Guid carteiraId)
+        public async Task<ActionResult<CarteiraDigital>> ConsultarSaldoDaCarteira(Guid usuarioId, Guid carteiraId)
         {
             if (!ModelState.IsValid)
             {
@@ -58,25 +66,20 @@ namespace DigitalWalletManagerAPI.Controllers
 
             ServiceResult<CarteiraDigital?> serviceResult = await _consultarSaldoDaCarteiraService.ObterSaldoCarteiraUsuario(usuarioId, carteiraId);
 
-            if (serviceResult.Value == null || serviceResult.Value.Saldo == 0m)
-            {
-                const string message = "Saldo insuficiente";
-                throw new SaldoInsuficienteException(message);
-            }
 
             if (!serviceResult.Sucess)
             {
                 return Problem(statusCode:StatusCodes.Status500InternalServerError);
             }
 
-            return serviceResult.Value.Saldo;
+            return serviceResult.Value;
         }
 
 
-        [HttpPut("{usuarioId:Guid}/{carteiraId:Guid}")]
+        [HttpPut("adicionar-saldo")]
         [ProducesResponseType(typeof(ServiceResult<CarteiraDigital>), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<ActionResult<CarteiraDigital?>> AdicionarSaldoCarteira(Guid usuarioId, Guid carteiraId)
+        public async Task<ActionResult<CarteiraDigital?>> AdicionarSaldoCarteira(AdicionarSaldoDto adicionarSaldoDto)
         {
             if (!ModelState.IsValid)
             {
@@ -84,14 +87,13 @@ namespace DigitalWalletManagerAPI.Controllers
                 return BadRequest(errors);
             }
 
-            ServiceResult<CarteiraDigital?> serviceResult = await _atualizarSaldoCarteiraService.AtualizarSaldoCarteira(usuarioId, carteiraId);
+            ServiceResult<CarteiraDigital?> serviceResult = await _atualizarSaldoCarteiraService.AtualizarSaldoCarteira(adicionarSaldoDto.UsuarioId, adicionarSaldoDto.CarteiraId, adicionarSaldoDto.Valor);
 
 
             if (!serviceResult.Sucess)
             {
                 return Problem(statusCode: StatusCodes.Status500InternalServerError);
             }
-
 
             return serviceResult.Value;
         }
