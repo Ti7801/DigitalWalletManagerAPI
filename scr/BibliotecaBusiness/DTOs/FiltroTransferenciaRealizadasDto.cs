66b4681448a5b2bb174691aeffc3db6fad7212b0
Ti7801 <tiagodaltro19@gmail.com>
2025-05-11 18:09:26 -0300

    Criando um novo Dto FiltroTransferenciaRealizadasDto

---
+++ scr/BibliotecaBusiness/DTOs/FiltroTransferenciaRealizadasDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaBusiness.DTOs
+{
+    public class FiltroTransferenciaRealizadasDto
+    {
+        public Guid UsuarioId { get; set; }
+        public Guid CarteiraId { get; set; }
+        public DateTime? DataInicio { get; set; }
+        public DateTime? DataFim { get; set; }
+    }
+}
