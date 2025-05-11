0e2ac92c59736c2ae4951b55adb816a1eddbd100
Ti7801 <tiagodaltro19@gmail.com>
2025-05-11 17:09:12 -0300

    Criação de novas interfaces de Repositório

--- scr/BibliotecaBusiness/Abstractions/ITransferenciaRepository.cs
+++ scr/BibliotecaBusiness/Abstractions/ITransferenciaRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BibliotecaBusiness.DTOs;
+using BibliotecaBusiness.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,7 @@ namespace BibliotecaBusiness.Abstractions
 {
     public interface ITransferenciaRepository
     {
+        Task<Transferencia?> CriarTransferencia(Transferencia transferencia);
+        Task<List<Transferencia>> ObterTransferenciasPorCarteiraAsync(Guid carteiraId, DateTime? dataInicio, DateTime? dataFim);
     }
 }
