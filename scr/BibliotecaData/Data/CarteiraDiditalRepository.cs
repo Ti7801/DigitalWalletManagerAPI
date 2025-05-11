296ecf8dfcb650247eb657e144d952d974c96aa9
Ti7801 <tiagodaltro19@gmail.com>
2025-05-11 00:12:43 -0300

    Criação dos métodos no repositório de CarteiraDigitalRepository: AdicionarCarteiraDigital, ObterCarteiraDigital,AtualizarCarteiraDigital.

--- scr/BibliotecaData/Data/CarteiraDiditalRepository.cs
+++ scr/BibliotecaData/Data/CarteiraDiditalRepository.cs
@@ -1,4 +1,7 @@
 ﻿using BibliotecaBusiness.Abstractions;
+using BibliotecaBusiness.Exceptions;
+using BibliotecaBusiness.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +12,49 @@ namespace BibliotecaData.Data
 {
     public class CarteiraDiditalRepository : ICarteiraDigitalRepository
     {
+        private readonly AppDbContext _appDbContext; 
+
+        public CarteiraDiditalRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public async Task AdicionarCarteiraDigital(CarteiraDigital carteira)
+        {
+            _appDbContext.Carteiras.Add(carteira);  
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        public async Task<CarteiraDigital?> ObterCarteiraDigital(Guid usuarioId, Guid carteiraId)
+        {
+            var listCarteira = await _appDbContext.Carteiras.Where(x => x.UsuarioId == usuarioId).ToListAsync();
+
+            if (listCarteira == null)
+            {
+                const string message = "Carteiras não foram encontradas para o usuário!";
+
+                throw new UsuarioNaoEncontradoException(message);
+            }
+
+            var carteira = listCarteira.Where(x => x.Id == carteiraId).SingleOrDefault();
+
+            return carteira;
+        }
+
+        public async Task<CarteiraDigital?> AtualizarSaldoCarteira(Guid usuarioId, Guid carteiraId)
+        {
+            var carteira = await ObterCarteiraDigital(usuarioId, carteiraId);
+
+            var carteiraAtualizada = new CarteiraDigital
+            {
+                Id = carteira.Id,
+                Saldo = carteira.Saldo,
+                UsuarioId = usuarioId  
+            };
+
+            await AdicionarCarteiraDigital(carteiraAtualizada);
+
+            return carteiraAtualizada;  
+        }
     }
 }
