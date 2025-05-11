30f71a69eaad5235fc10f154175cbc437b1ed280
Ti7801 <tiagodaltro19@gmail.com>
2025-05-11 17:10:19 -0300

    Excluindo a propriedade confirmação de senha do Dto de usuário.

--- scr/BibliotecaBusiness/DTOs/UsuarioDto.cs
+++ scr/BibliotecaBusiness/DTOs/UsuarioDto.cs
@@ -26,11 +26,5 @@ namespace BibliotecaBusiness.DTOs
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(maximumLength: 30, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
         public string Password { get; set; } = string.Empty;
-
-        [Display(Name = "confirmpassword")]
-        [Required(ErrorMessage = "O campo {0} é obrigatório")]
-        [Compare(nameof(Password), ErrorMessage = "As senhas não conferem")]
-        public string ConfirmPassword { get; set; } = string.Empty;
-
     }
 }
