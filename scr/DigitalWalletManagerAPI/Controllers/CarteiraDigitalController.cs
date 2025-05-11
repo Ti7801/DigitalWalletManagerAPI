b6f301e731a372c84247c7e89826de025ff4a720
Ti7801 <tiagodaltro19@gmail.com>
2025-05-11 00:13:43 -0300

    Construindo as Actions referêntes as funcionalidades da CarteiraDigital.

--- scr/DigitalWalletManagerAPI/Controllers/CarteiraDigitalController.cs
+++ scr/DigitalWalletManagerAPI/Controllers/CarteiraDigitalController.cs
@@ -1,9 +1,99 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BibliotecaBusiness.Exceptions;
+using BibliotecaBusiness.Models;
+using BibliotecaBusiness.Services;
+using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace DigitalWalletManagerAPI.Controllers
 {
+    [ApiController]
+    [Route("api/carteiracontroller")]
     public class CarteiraDigitalController : ControllerBase
     {
-        public CarteiraDigitalController() { }
+        private readonly CriarCarteiraDigitalService _criarCarteiraDigitalService;
+        private readonly ConsultarSaldoDaCarteiraService _consultarSaldoDaCarteiraService;
+        private readonly AtualizarSaldoCarteiraService _atualizarSaldoCarteiraService;
+
+        public CarteiraDigitalController(CriarCarteiraDigitalService criarCarteiraDigitalService,
+                                         ConsultarSaldoDaCarteiraService consultarSaldoDaCarteiraService,
+                                         AtualizarSaldoCarteiraService atualizarSaldoCarteiraService) 
+        {
+            _criarCarteiraDigitalService = criarCarteiraDigitalService;
+            _consultarSaldoDaCarteiraService = consultarSaldoDaCarteiraService;
+            _atualizarSaldoCarteiraService = atualizarSaldoCarteiraService;
+
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(CarteiraDigital), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> CriarCarteiraDigital(CarteiraDigital carteira)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).SingleOrDefault();
+                return BadRequest(errors);
+            }
+
+            ServiceResult serviceResult = await _criarCarteiraDigitalService.CadastrarCarteiraDigital(carteira);
+
+            if (!serviceResult.Success)
+            {
+                return Problem(statusCode:StatusCodes.Status500InternalServerError);
+            }
+
+            return CreatedAtAction(nameof(CriarCarteiraDigital), carteira.Id);
+        }
+
+        [HttpGet("{usuarioId:Guid}/{carteiraId:Guid}")]
+        [ProducesResponseType(typeof(decimal?), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<decimal?>> ConsultarSaldoDaCarteira(Guid usuarioId, Guid carteiraId)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).SingleOrDefault();
+                return BadRequest(errors);
+            }
+
+            ServiceResult<CarteiraDigital?> serviceResult = await _consultarSaldoDaCarteiraService.ObterSaldoCarteiraUsuario(usuarioId, carteiraId);
+
+            if (serviceResult.Value == null || serviceResult.Value.Saldo == 0m)
+            {
+                const string message = "Saldo insuficiente";
+                throw new SaldoInsuficienteException(message);
+            }
+
+            if (!serviceResult.Sucess)
+            {
+                return Problem(statusCode:StatusCodes.Status500InternalServerError);
+            }
+
+            return serviceResult.Value.Saldo;
+        }
+
+
+        [HttpPut("{usuarioId:Guid}/{carteiraId:Guid}")]
+        [ProducesResponseType(typeof(ServiceResult<CarteiraDigital>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<CarteiraDigital?>> AdicionarSaldoCarteira(Guid usuarioId, Guid carteiraId)
+        {
+            if (!ModelState.IsValid)
+            {
+                var errors = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).SingleOrDefault();
+                return BadRequest(errors);
+            }
+
+            ServiceResult<CarteiraDigital?> serviceResult = await _atualizarSaldoCarteiraService.AtualizarSaldoCarteira(usuarioId, carteiraId);
+
+
+            if (!serviceResult.Sucess)
+            {
+                return Problem(statusCode: StatusCodes.Status500InternalServerError);
+            }
+
+
+            return serviceResult.Value;
+        }
     }
 }
