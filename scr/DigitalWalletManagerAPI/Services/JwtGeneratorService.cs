985330534e579021ea6bfda3d122ffda09f43591
Ti7801 <tiagodaltro19@gmail.com>
2025-05-13 13:13:00 -0300

    Criando novos serviços para implementar a configuração de autenticação.

---
+++ scr/DigitalWalletManagerAPI/Services/JwtGeneratorService.cs
@@ -0,0 +1,36 @@
+﻿using BibliotecaBusiness.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+
+namespace DigitalWalletManagerAPI.Services
+{
+    public class JwtGeneratorService
+    {
+        private readonly JwtSettings _appSettings;
+
+        public JwtGeneratorService(JwtSettings appSettings) 
+        {
+            _appSettings = appSettings;
+        }
+
+
+        public string GerarJwt()
+        {
+            var key = Encoding.ASCII.GetBytes(_appSettings.Segredo);
+            var tokenHandler = new JsonWebTokenHandler();
+            string token = tokenHandler.CreateToken(new SecurityTokenDescriptor()
+            {
+                Issuer = _appSettings.Emissor,
+                Audience = _appSettings.Audiencia,
+                Expires = DateTime.UtcNow.AddHours(_appSettings.ExpiracaoHoras),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            });
+
+            return token;
+        }
+    }
+}
