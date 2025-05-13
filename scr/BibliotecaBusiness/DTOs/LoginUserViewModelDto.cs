4c095affbc43c2dc88dfb23a8618ce61647fd875
Ti7801 <tiagodaltro19@gmail.com>
2025-05-13 13:14:38 -0300

    Criando arquivos de modelos Dto para ajudar na configuração de autenticação.

---
+++ scr/BibliotecaBusiness/DTOs/LoginUserViewModelDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaBusiness.DTOs
+{
+    public class LoginUserViewModelDto
+    {
+        [Display(Name = "email")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [EmailAddress(ErrorMessage = "O campo {0} está formato inválido")]
+        public string Email { get; set; }
+
+        [Display(Name = "password")]
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter {2} e {1} caracteres", MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+}
